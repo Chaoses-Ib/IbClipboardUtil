31874ab62cbc3348669466bd2d01b2bc656649e3
Chaoses Ib <Chaos-es@outlook.com>
2021-05-16 19:27:35 +0800

    Improve GetName: support standard clipboard formats

--- IbClipboardUtil/Clipboard.cs
+++ IbClipboardUtil/Clipboard.cs
@@ -42,9 +42,39 @@ namespace Ib.Windows.DataExchange
 
         public string GetName()
         {
-            var buf = new StringBuilder(80);
-            buf.Length = ClipboardNative.GetClipboardFormatName(format, buf, buf.Capacity);
-            return buf.ToString();
+            switch (format)
+            {
+                case 1: return "CF_TEXT";
+                case 2: return "CF_BITMAP";
+                case 3: return "CF_METAFILEPICT";
+                case 4: return "CF_SYLK";
+                case 5: return "CF_DIF";
+                case 6: return "CF_TIFF";
+                case 7: return "CF_OEMTEXT";
+                case 8: return "CF_DIB";
+                case 9: return "CF_PALETTE";
+                case 10: return "CF_PENDATA";
+                case 11: return "CF_RIFF";
+                case 12: return "CF_WAVE";
+                case 13: return "CF_UNICODETEXT";
+                case 14: return "CF_ENHMETAFILE";
+                case 15: return "CF_HDROP";
+                case 16: return "CF_LOCALE";
+                case 17: return "CF_DIBV5";
+                case 0x80: return "CF_OWNERDISPLAY";
+                case 0x81: return "CF_DSPTEXT";
+                case 0x82: return "CF_DSPBITMAP";
+                case 0x83: return "CF_DSPMETAFILEPICT";
+                case 0x8E: return "CF_DSPENHMETAFILE";
+                case 0x200: return "CF_PRIVATEFIRST";
+                case 0x2FF: return "CF_PRIVATELAST";
+                case 0x300: return "CF_GDIOBJFIRST";
+                case 0x3FF: return "CF_GDIOBJLAST";
+                default:
+                    var buf = new StringBuilder(80);
+                    buf.Length = ClipboardNative.GetClipboardFormatName(format, buf, buf.Capacity);
+                    return buf.ToString();
+            }
         }
 
         public byte[] GetData()
